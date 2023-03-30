712c4fcff3b47ab790e17330c2b98314e477c506
MokoneSA <simonlephotojr@gmail.com>
2023-03-30 16:59:14 +0000

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script that sends a URL request and displays the size
-# of the body of the response.
-
-curl -s "$1" | wc -c 
+# Script that sends a URL request and displays the size of the body of the response.
+curl -s "$1" | wc -c
